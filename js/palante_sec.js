117006fd7bb602ea52775c64f6e1f7fcee787f37
Kristin Snelling <kristin@palantetech.coop>
2020-04-14 22:04:37 -0400

    Theme styling, js adjustments, adding images

--- js/palante_sec.js
+++ js/palante_sec.js
@@ -2,4 +2,20 @@
  $(document).ready(function () {
     $('#edit-field-organization-state-tid').SumoSelect({search: true, searchText: 'Search here.', placeholder: 'Select state(s)...', csvDispCount: 3 });
     $('#edit-field-topic-tid').SumoSelect({search: true, searchText: 'Search here', placeholder: 'Select topic(s)...', csvDispCount: 3 });
+    $('#edit-type').SumoSelect({search: true, searchText: 'Search here', placeholder: 'Select type(s)...', csvDispCount: 3 });
+    $('#edit-field-age-group-tid').SumoSelect({search: true, searchText: 'Search here', placeholder: 'Select age(s)...', csvDispCount: 3 });
+	$('.views-field-field-type-of-training .field-content:contains("person")').html(function(_, html) {
+		return html.replace(/(In person)/g, '<span class="inperson">$1</span>');
+	});
+	$('.views-field-field-type-of-training .field-content:contains("Online")').html(function(_, html) {
+		return html.replace(/(Online)/g, '<span class="online">$1</span>');
+	});
+	$('.drawer').each(function () {
+		$(this).children('.plsse-hide').appendTo($(this).children('.pull'));
+	  	if($(this).find('.junk').length == 0) {
+			$(this).children( '.plsse-hide' ).hide();
+		}
+  	});
  });
+ 
+ 
