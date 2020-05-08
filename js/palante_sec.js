5c524327444cecd584e42989f7b178048786e96a
Kristin Snelling <kristin@palantetech.coop>
2020-05-08 17:40:14 -0400

    Added page template, adjustments to css and js

--- js/palante_sec.js
+++ js/palante_sec.js
@@ -1,4 +1,7 @@
  // SEC-specific CSS (needs to move to its own theme)
+$(window).on('load', function (e) {
+	$('#loader').delay(300).fadeOut();
+});
  $(document).ready(function () {
     $('#edit-field-organization-state-tid').SumoSelect({search: true, searchText: 'Search here.', placeholder: 'Select state(s)...', csvDispCount: 3 });
     $('#edit-field-topic-tid').SumoSelect({search: true, searchText: 'Search here', placeholder: 'Select topic(s)...', csvDispCount: 3 });
