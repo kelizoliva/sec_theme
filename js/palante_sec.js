283b1e18474bd2f137d61302a79efd39977f6b10
Kristin Elizoliva <kristin@palantetech.coop>
2020-08-24 08:16:14 -0700

    Added portal view secindary results template, secondary shrinker styling, work on getting js to fire in ajax loaded content

--- js/palante_sec.js
+++ js/palante_sec.js
@@ -1,5 +1,34 @@
 $(window).on('load', function (e) {
 	$('#loader').delay(300).fadeOut();
+});
+$(document).ready(function () {
+	$('.shrinker').append('<span>Search<span>');
+    $('#edit-field-organization-state-tid').SumoSelect({search: true, searchText: 'Search here.', placeholder: 'Select state(s)...', csvDispCount: 3 });
+    $('#edit-field-topic-tid').SumoSelect({search: true, searchText: 'Search here', placeholder: 'Select topic(s)...', csvDispCount: 3 });
+    $('#edit-type').SumoSelect({search: true, searchText: 'Search here', placeholder: 'Select type(s)...', csvDispCount: 3 });
+    $('#edit-field-age-group-tid').SumoSelect({search: true, searchText: 'Search here', placeholder: 'Select age(s)...', csvDispCount: 3 });
+    $('#edit-field-type-of-training-value').SumoSelect({search: true, searchText: 'Search here', placeholder: 'In Person or Online...', csvDispCount: 3 });
+	$('.views-field-field-type-of-training .field-content:contains("person")').html(function(_, html) {
+		return html.replace(/(In person)/g, '<span class="inperson">$1</span>');
+	});
+	$('.views-field-field-type-of-training .field-content:contains("Online")').html(function(_, html) {
+		return html.replace(/(Online)/g, '<span class="online">$1</span>');
+	});
+  	$('.node-type-organization .block.block-field-node-field-trainings-webpage .field-item  a').each(function () {
+	  	$(this).addClass('button');
+  	});
+  	$('.node-type-organization .block.block-field-node-field-trainings-webpage .field-item  a:contains("Trainings Webpage"), .views-field-field-trainings-webpage .button a:contains("Trainings Webpage")').html(function(_, html) {
+		return html.replace(/(Trainings Webpage)/g, 'Visit Trainings Webpage');
+	});
+	// Hide list of States if this is a national organization
+	$('.National-Organization .field-content').each(function() {
+	   $(this).html('All States and Territories');
+	});
+	if($('#edit-field-organization-state-tid').val()){
+    	$('.block-views-portal-offerings-search-block-1').removeClass('hide');
+	} else {
+    	$('.block-views-portal-offerings-search-block-1').addClass('hide');
+	}
 	
 	// populate limiting filter
 	var limiter = jQuery('#edit-field-organization-state-tid-1');
@@ -26,40 +55,36 @@ $(window).on('load', function (e) {
 		    });
 		});
 	});
-	limiter.trigger('change');
-});
-$(document).ready(function () {
-	$('.shrinker').append('<span>Search<span>');
-    $('#edit-field-organization-state-tid').SumoSelect({search: true, searchText: 'Search here.', placeholder: 'Select state(s)...', csvDispCount: 3 });
-    $('#edit-field-topic-tid').SumoSelect({search: true, searchText: 'Search here', placeholder: 'Select topic(s)...', csvDispCount: 3 });
-    $('#edit-type').SumoSelect({search: true, searchText: 'Search here', placeholder: 'Select type(s)...', csvDispCount: 3 });
-    $('#edit-field-age-group-tid').SumoSelect({search: true, searchText: 'Search here', placeholder: 'Select age(s)...', csvDispCount: 3 });
-    $('#edit-field-type-of-training-value').SumoSelect({search: true, searchText: 'Search here', placeholder: 'In Person or Online...', csvDispCount: 3 });
-	$('.views-field-field-type-of-training .field-content:contains("person")').html(function(_, html) {
-		return html.replace(/(In person)/g, '<span class="inperson">$1</span>');
-	});
-	$('.views-field-field-type-of-training .field-content:contains("Online")').html(function(_, html) {
-		return html.replace(/(Online)/g, '<span class="online">$1</span>');
-	});
+	limiter.trigger('change');	
+
+	/* plsse drawer functionality
+	$('.shrinkable').load(link.attr('href'),function(responseText, textStatus, XMLHttpRequest){
+    	$('.drawer').each(function () {
+	    e.preventDefault();
+		  $(this).children('.pull').on("click", function(){
+			$(this).toggleClass('open');
+		    $(this).parent().children('.junk').slideToggle();
+		    return false;
+		  });
+	  	});
+		$('.drawer').each(function () {
+			$(this).children('.plsse-hide').appendTo($(this).children('.pull'));
+		  	if($(this).find('.junk').length == 0) {
+				$(this).children( '.plsse-hide' ).hide();
+			}
+			});
+	}); */
+	$('.drawer').each(function () {
+	  $(this).children('.pull').on("click", function(){
+		$(this).toggleClass('open');
+	    $(this).parent().children('.junk').slideToggle();
+	    return false;
+	  });
+  	});
 	$('.drawer').each(function () {
 		$(this).children('.plsse-hide').appendTo($(this).children('.pull'));
 	  	if($(this).find('.junk').length == 0) {
 			$(this).children( '.plsse-hide' ).hide();
 		}
   	});
-  	$('.node-type-organization .block.block-field-node-field-trainings-webpage .field-item  a').each(function () {
-	  	$(this).addClass('button');
-  	});
-  	$('.node-type-organization .block.block-field-node-field-trainings-webpage .field-item  a:contains("Trainings Webpage"), .views-field-field-trainings-webpage .button a:contains("Trainings Webpage")').html(function(_, html) {
-		return html.replace(/(Trainings Webpage)/g, 'Visit Trainings Webpage');
-	});
-	// Hide list of States if this is a national organization
-	$('.National-Organization .field-content').each(function() {
-	   $(this).html('All States and Territories');
-	});
-	if($('#edit-field-organization-state-tid').val()){
-    	$('.block-views-portal-offerings-search-block-1').removeClass('hide');
-	} else {
-    	$('.block-views-portal-offerings-search-block-1').addClass('hide');
-	}
 });
